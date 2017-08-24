11818d9792f7015db3d7f212053927760e3ccaca
anderson96c <anderson.ivanildo@hotmail.com>
2017-08-24 01:39:17 -0300

    Fazer Ex03

--- Ex02/Pizza.java
+++ Ex02/Pizza.java
@@ -1,9 +1,25 @@
-
+/**
+*
+* Classe Pizza
+*
+* Herdeira da classe "ProdutoNaoDuravel"
+*
+* Acrescenta atributos 'sabor' e 'pedaços'
+* e métodos para esses atributos
+*
+*@Autor Anderson Caio da Fonseca Santos 
+*/
 public class Pizza extends ProdutoNaoDuravel
 {
+	//Atributos de uma Pizza
 	private String sabor;
 	private int pedacos;
 
+	/**
+	 * Método construtor da classe
+	 *
+	 * Construtor 'super' herdado da classe ProdutoNaoDuravel
+	 */
 	public Pizza(String nome_, double preco_, String marca_, String descricao_, String data_fab_,
                               String dataVal_, String genero_, String sabor_, int pedacos_)
 	{
@@ -14,20 +30,33 @@ public class Pizza extends ProdutoNaoDuravel
 		pedacos = pedacos_;
 	}
 
+
 	/**
-	Sem sets jah que nao faz sentido mudar o sabor 
-	ou tamanho de uma pizza depois dela estar pronta
-	*/
+	 *Retorna o sabor da Pizza
+	 *
+	 *@return	String		sabor da pizza
+	 */
 
 	public String getSabor()
 	{
 		return sabor;
 	}
+	
+	/**
+	 *Retorna a quantidade de padeços restantes da Pizza
+	 *
+	 *@return	int		quantidade de padeços restantes da Pizza
+	 */
 
 	public int getPedacos()
 	{
 		return pedacos;
 	}
+
+	/**
+	 *Consome um pedaço da pizza caso ainda restem pedaços
+	 *caso nao haja mais pedaços avisa que a pizza acabou
+	 */
 	
 	public void eatSlice()
 	{
--- Ex02/Refrigerante.java
+++ Ex02/Refrigerante.java
@@ -1,9 +1,25 @@
-
+/**
+*
+* Classe Pizza
+*
+* Herdeira da classe "ProdutoNaoDuravel"
+*
+* Acrescenta atributos 'sabor' e 'litros'
+* e métodos para esses atributos
+*
+*@Autor Anderson Caio da Fonseca Santos 
+*/
 public class Refrigerante extends ProdutoNaoDuravel
 {
+	//Atributos de um Refrigerante
 	private String sabor;
 	private double litros;
-
+	
+	/**
+	 * Método construtor da classe
+	 *
+	 * Construtor 'super' herdado da classe ProdutoNaoDuravel
+	 */
 	public Refrigerante(String nome_, double preco_, String marca_, String descricao_, String data_fab_,
                             String dataVal_, String genero_, String sabor_, double litros_)
 	{
@@ -13,17 +29,31 @@ public class Refrigerante extends ProdutoNaoDuravel
 		sabor = sabor_;
 		litros = litros_;
 	}
+	
+	/**
+	 *Retorna o sabor do Refrigenrante
+	 *
+	 *@return	String		sabor do refrigerante
+	 */
 
 	public String getSabor()
 	{
 		return sabor;
 	}
 
+	/**
+	 *Retorna a quantidade de litros restantes de refrigerante
+	 *
+	 *@return	double		litros restantes de refrigerante
+	 */
+
 	public double getLitros()
 	{
 		return litros;
 	}
-	
+	/**
+	 *Consome 500ml de refrigerante
+	 */
 	public void drink()
 	{
 		if(litros <= 0)
